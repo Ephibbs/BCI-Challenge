84fe4844b16f791ac80524b38bdd04a4da3313d8
Ethan Bian <epiary@gmail.com>
2014-12-11 00:16:48 -0600

    Change convert to to work with 'test' too

--- convert.py
+++ convert.py
@@ -2,12 +2,26 @@ import csv
 import cPickle
 import numpy
 import os
+import sys
 
 
-# Convert all training-set .csv files into a numpy binary data file
-for file_name in os.listdir("./data/train"):
-	print "Processing: " + file_name
+def convert(in_dir, out_dir):
+    for file_name in os.listdir(in_dir):
+        print "Processing: " + file_name
 
-	numpy_arr = numpy.genfromtxt("./data/train/" + file_name, delimiter=",")
+        numpy_arr = numpy.genfromtxt(in_dir + file_name, delimiter=",")
 
-	numpy.save("./data_processed/" + file_name.replace(".csv", ".npy"), numpy_arr)
+        numpy.save(out_dir + file_name.replace(".csv", ".npy"), numpy_arr)
+
+    print "Finished converting."
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print "Please enter an argument of either \"train\" or \"test\"."
+        sys.exit()
+
+    if sys.argv[1] == "train":
+        convert("./data/train/", "./data_processed/train/")
+    elif sys.argv[1] == "test":
+        convert("./data/test/", "./data_processed/test/")
