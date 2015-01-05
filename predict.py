b64e34f1abb176e9030792fc26b251560282e87d
Evan Phibbs <evan.phibbs@yahoo.com>
2015-01-05 12:54:31 -0800

    This can now be run without reparsing the data each time. Also, it is easy to pick which channels, duration, and algorithm to use`

--- predict.py
+++ predict.py
@@ -5,7 +5,12 @@ import time
 import numpy as np
 import pandas as pd
 import sklearn.ensemble as ens
+from sklearn import metrics
 
+def score(classifier, X, Y):
+    fpr, tpr, thresholds = metrics.roc_curve(Y, classifier.predict(X), pos_label=1)
+    auc = metrics.auc(fpr,tpr)
+    return auc
 
 def labelled_array(y, column_labels=[]):
     '''Create a structured array (numpy array with labelled columns).
@@ -30,110 +35,95 @@ def get_column(np_array, label):
 
     return np_array[:, column[label]]
 
+def parse_data(dirName, subs, channels, duration):
+    #helper function parses data and saves to numpy
+    output = pd.DataFrame(columns=['subject','session','feedback_num','start_pos'] + reduce(lambda x,y: x+y, [[channel+'_' + s for s in map(str,range(duration+1))] for channel in channels]),index=range(5440))
+    counter = 0
+    data = {}
+    for i in subs:
+        for j in range(1,6):
+            stime = time.time()
+            # Load in data
+            # temp = pd.read_csv('train/Data_S' + i + '_Sess0'  + str(j) + '.csv')
+            temp = np.load('data/data_processed/'+dirName+'/Data_S' + i + '_Sess0'  + str(j) + '.npy')
+            temp = np.delete(temp, (0), axis=0) # delete first row (labels)
 
-train_subs = ['02','06','07','11','12','13','14','16','17','18','20','21','22','23','24','26']
-test_subs = ['01','03','04','05','08','09','10','15','19','25']
+            # Get the positions of the non-zero FeedBackEvents
+            # fb = temp.query('FeedBackEvent == 1',engine='python')['FeedBackEvent']
+            fb = np.flatnonzero(get_column(temp, "FeedBackEvent"))
+            
+            counter2 = 0
+            for k in fb: # replaced "fb.index" with "fb"
+                # temp2 = temp.loc[int(k):int(k)+260,'Cz']
+                temp2 = pd.Series(reduce(lambda x,y: x+y, [get_column(temp, channel)[k:k+duration + 1] for channel in channels]))
 
-start_time = time.time()
+                temp2.index = reduce(lambda x,y: x+y, [[channel+'_' + s for s in map(str,range(duration+1))] for channel in channels])
+                output.loc[counter,reduce(lambda x,y: x+y, [[channel+'_' + s for s in map(str,range(duration+1))] for channel in channels])] = temp2
+                output.loc[counter,'session'] = j
+                output.loc[counter, 'subject'] = i
+                output.loc[counter, 'feedback_num'] = counter2
+                output.loc[counter, 'start_pos'] = k
+                counter +=1
+                counter2 +=1
 
-train_labels = pd.read_csv('data/TrainLabels.csv')
-submission = pd.read_csv('data/SampleSubmission.csv')
+            print time.time() - stime
 
-train = pd.DataFrame(columns=['subject','session','feedback_num','start_pos'] + ['Cz_' + s for s in map(str,range(261))],index=range(5440))
+        print 'subject ', i
 
-# trainz = labelled_array(5440, ['subject', 'session', 'feedback_num', 'start_pos'] + ['Cz_' + s for s in map(str,range(261))]) #######################
+    #output.to_csv(dirName+'.csv',ignore_index=True)
+    np.save(dirName+'.csv', output.values.astype(float))
 
-
-counter = 0
-print '========train data========'
-data = {}
-for i in train_subs:
-    for j in range(1,6):
-        stime = time.time()
-        # Load in data
-        # temp = pd.read_csv('train/Data_S' + i + '_Sess0'  + str(j) + '.csv')
-        temp = np.load('data/data_processed/train/Data_S' + i + '_Sess0'  + str(j) + '.npy')
-        temp = np.delete(temp, (0), axis=0) # delete first row (labels)
-
-        # Get the positions of the non-zero FeedBackEvents
-        # fb = temp.query('FeedBackEvent == 1',engine='python')['FeedBackEvent']
-        fb = np.flatnonzero(get_column(temp, "FeedBackEvent"))
-        
-        counter2 = 0
-        for k in fb: # replaced "fb.index" with "fb"
-            # temp2 = temp.loc[int(k):int(k)+260,'Cz']
-            temp2 = pd.Series(get_column(temp, "Cz")[k:k+260 + 1])
-
-            temp2.index = ['Cz_' + s for s in map(str,range(261))]
-            train.loc[counter,['Cz_' + s for s in map(str,range(261))]] = temp2
-            train.loc[counter,'session'] = j
-            train.loc[counter, 'subject'] = i
-            train.loc[counter, 'feedback_num'] = counter2
-            train.loc[counter, 'start_pos'] = k
-
-
-            # trainz[counter] = np.concatenate((
-            #     [i, j, counter2, k],
-            #     get_column(temp, "Cz")[k:k+260 + 1].T
-            # ), axis=1)
+    return output.values.astype(float)
 
 
-            counter +=1
-            counter2 +=1
+def get_data(redo=1, channels=["Cz"], duration=260):
+    #redo: reparse the data
+    #channels: channels to use
+    #duration: number of timesteps of each channel starting with the feedback event
+    if redo==0: 
+        return np.load("train.npy"), np.load("test.npy")
 
-        print time.time() - stime
+    train_subs = ['02','06','07','11','12','13','14','16','17','18','20','21','22','23','24','26']
+    test_subs = ['01','03','04','05','08','09','10','15','19','25']
 
-    print 'subject ', i
+    print '========train data========'
+    train = parse_data("train", train_subs, channels, duration)
 
-train.to_csv('train_cz.csv',ignore_index=True)
+    print '========test data========'
+    test = parse_data("test", test_subs, channels, duration)
 
-# np.savetxt("foo.csv", trainz, delimiter=",", header=", ".join(trainz.dtype.names), comments="") ########################
+    return train, test
 
+def predictions_to_file(algo, train, train_labels, test):
+    algo.fit(train, train_labels)
+    preds = algo.predict_proba(test)
+    preds = preds[:,1]
+    submission['Prediction'] = preds
+    submission.to_csv('prediction.csv',index=False)
 
-test = pd.DataFrame(columns=['subject','session','feedback_num','start_pos'] + ['Cz_' + s for s in map(str,range(261))],index=range(3400))
-print '========test data========'
-counter = 0
-data = {}
-for i in test_subs:
-    for j in range(1,6):
-        stime = time.time()
-        # temp = pd.read_csv('test/Data_S' + i + '_Sess0'  + str(j) + '.csv')
-        temp = np.load('data/data_processed/test/Data_S' + i + '_Sess0'  + str(j) + '.npy')
-        temp = np.delete(temp, (0), axis=0) # delete first row (labels)
+if __name__ == "__main__":
+    start_time = time.time()
 
-        # Get the positions of the non-zero FeedBackEvents
-        # fb = temp.query('FeedBackEvent == 1',engine='python')['FeedBackEvent']     
-        fb = np.flatnonzero(get_column(temp, "FeedBackEvent"))
+    submission = pd.read_csv('data/SampleSubmission.csv')
 
-        counter2 = 0
-        for k in fb:
-            # temp2 = temp.loc[int(k):int(k)+260,'Cz']
-            temp2 = pd.Series(get_column(temp, "Cz")[k:k+260 + 1])
+    train, test = get_data(0, ["Cz"], 130)
+    train_labels = pd.read_csv('data/TrainLabels.csv').values[:,1].astype(float).ravel()
 
-            temp2.index = ['Cz_' + s for s in map(str,range(261))]
-            test.loc[counter,['Cz_' + s for s in map(str,range(261))]] = temp2
-            test.loc[counter,'session'] = j
-            test.loc[counter, 'subject'] = i
-            test.loc[counter, 'feedback_num'] = counter2
-            test.loc[counter, 'start_pos'] = k
-            counter +=1  
-            counter2 +=1
+    print 'training GBM'
 
-        print time.time() - stime
+    #Probably better to set max_features to default which is sqrt(n_features) or just delete that part, the current value was left in by accident, and probably is not great
 
-    print 'subject ', i  
+    ###     Algorithms      ###
+    #algo = ens.AdaBoostClassifier(n_estimators=500, learning_rate=0.05)
+    algo = ens.GradientBoostingClassifier(n_estimators=500, learning_rate=0.05)
 
-test.to_csv('test_cz.csv',ignore_index=True)
+    split = len(train)/2
+    algo.fit(train[:split], train_labels[:split])
+    pred = algo.predict(train[split:])
+    print(metrics.classification_report(train_labels[split:], pred))
+    print algo.score(train[split:], train_labels[split:])
+    #print score(algo, train[split:], train_labels[split:])
 
-print 'training GBM'
-
-
-#Probably better to set max_features to default which is sqrt(n_features) or just delete that part, the current value was left in by accident, and probably is not great
-gbm = ens.GradientBoostingClassifier(n_estimators=500,learning_rate=0.05, max_features=0.25)
-gbm.fit(train, train_labels.values[:,1].ravel())
-preds = gbm.predict_proba(test)
-preds = preds[:,1]
-submission['Prediction'] = preds
-submission.to_csv('prediction.csv',index=False)
-print 'Done'
-print time.time() - start_time
+    #predictions_to_file(algo, train, train_labels, test)
+    print 'Done'
+    print time.time() - start_time
