a898bd0c94d3421b08d0d6bfbb924a773a528ee1
Evan Phibbs <evan.phibbs@yahoo.com>
2015-02-06 02:16:43 -0600

    dsf

--- predict.py
+++ predict.py
@@ -15,6 +15,7 @@ from sklearn.tree import DecisionTreeClassifier
 from scipy.signal import filtfilt, butter
 from time import time
 from sklearn.decomposition import RandomizedPCA
+from pandas import pd
 
 def score(classifier, X, y):
     fpr, tpr, thresholds = metrics.roc_curve(y, classifier.predict(X), pos_label=1)
